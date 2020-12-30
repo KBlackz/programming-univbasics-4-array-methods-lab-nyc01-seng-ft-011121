49e3584a9cdbb63ebb3cf886fceb985cd5b4472b
Karris  Black <karris.b@gmail.com>
2020-12-30 22:31:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- famous_cats = ["lil' bub", "grumpy cat", "maru"]
+ famous_cats = ["lil' bub", "grumpy cat", "wow"]
   famous_cats.sort
   
 end
